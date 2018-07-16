57b35fede9d65116d3fce72bb8814c43aca58ecd
Siddharth Avadhanam <sna53@cbsubscb09.tc.cornell.edu>
2018-07-15 20:15:31 -0400

    deleted files with -A flag in git add

--- pReg.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-import math
-import sys
-
-pedsimIDFILE = "~/siddharth/AncestryInference/Results/admix60/admix60-output.ids"
-idDF = pd.read_table(pedsimIDFILE,header=None,names=['simids','popids'])
-
-
